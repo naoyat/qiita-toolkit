531febd9c29e5144cfa905c5556ae06429184943
naoya_t <naoya.tozuka@gmail.com>
2012-11-27 14:53:35 +0900

    不等号の実体参照に対応

--- TeX-on-Qiita/chrome/js/tex-on-qiita.js
+++ TeX-on-Qiita/chrome/js/tex-on-qiita.js
@@ -7,8 +7,11 @@ function tex2md() {
     md += RegExp.leftContext + RegExp.$1;
     var texsrc = RegExp.$2;
     text = RegExp.$3 + RegExp.rightContext;
-    texsrc = texsrc.replace(/\\\[/g,'[').replace(/\\\]/g,']');
-    md += "![equation](http://chart.apis.google.com/chart?cht=tx&chl=" + encodeURIComponent(texsrc).replace(/\)/g,'%29') + ")";
+    texsrc = texsrc.replace(/\\\[/g,'[').replace(/\\\]/g,']'); // \[, \]
+    texsrc = texsrc.replace(/\&lt;/g,'<').replace(/\&gt;/g,'>'); // &lt;, &gt;
+    texsrc = encodeURIComponent(texsrc);
+    texsrc = texsrc.replace(/\)/g,'%29');
+    md += "![equation](http://chart.apis.google.com/chart?cht=tx&chl=" + texsrc + ")";
   }
   md += text;
   $('#item-body')[0].value = md;
@@ -21,7 +24,10 @@ function md2tex() {
     text += RegExp.leftContext + RegExp.$1;
     var texsrc = RegExp.$2;
     md = RegExp.$3 + RegExp.rightContext;
-    text += "[tex:" + decodeURIComponent(texsrc).replace(/\[/g,'\\[').replace(/\]/g,'\\]') + "]";
+    texsrc = decodeURIComponent(texsrc);
+    texsrc = texsrc.replace(/\[/g,'\\[').replace(/\]/g,'\\]'); // \[, \]
+    // texsrc = texsrc.replace(/(\\lt|<)/g,'\&lt;').replace(/(\\gt|>)/g,'\&gt;'); // &lt; &gt;
+    text += "[tex:" + texsrc + "]";
   }
   text += md;
   $('#item-body')[0].value = text;
--- TeX-on-Qiita/firefox/tex-on-qiita.user.js
+++ TeX-on-Qiita/firefox/tex-on-qiita.user.js
@@ -4,7 +4,7 @@
 // @description Enables TeX-notation on Qiita
 // @include     http://qiita.com/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
-// @version     0.2
+// @version     0.3
 // ==/UserScript==
 
 (function(){
@@ -16,8 +16,11 @@ function tex2md() {
     md += RegExp.leftContext + RegExp.$1;
     var texsrc = RegExp.$2;
     text = RegExp.$3 + RegExp.rightContext;
-    texsrc = texsrc.replace(/\\\[/g,'[').replace(/\\\]/g,']');
-    md += "![equation](http://chart.apis.google.com/chart?cht=tx&chl=" + encodeURIComponent(texsrc).replace(/\)/g,'%29') + ")";
+    texsrc = texsrc.replace(/\\\[/g,'[').replace(/\\\]/g,']'); // \[, \]
+    texsrc = texsrc.replace(/\&lt;/g,'<').replace(/\&gt;/g,'>'); // &lt;, &gt;
+    texsrc = encodeURIComponent(texsrc);
+    texsrc = texsrc.replace(/\)/g,'%29');
+    md += "![equation](http://chart.apis.google.com/chart?cht=tx&chl=" + texsrc + ")";
   }
   md += text;
   $('#item-body')[0].value = md;
@@ -30,7 +33,10 @@ function md2tex() {
     text += RegExp.leftContext + RegExp.$1;
     var texsrc = RegExp.$2;
     md = RegExp.$3 + RegExp.rightContext;
-    text += "[tex:" + decodeURIComponent(texsrc).replace(/\[/g,'\\[').replace(/\]/g,'\\]') + "]";
+    texsrc = decodeURIComponent(texsrc);
+    texsrc = texsrc.replace(/\[/g,'\\[').replace(/\]/g,'\\]'); // \[, \]
+    // texsrc = texsrc.replace(/(\\lt|<)/g,'\&lt;').replace(/(\\gt|>)/g,'\&gt;'); // &lt; &gt;
+    text += "[tex:" + texsrc + "]";
   }
   text += md;
   $('#item-body')[0].value = text;
